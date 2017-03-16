ce92aaac81ce0a5ec1c8eb1a512571b6dff49664
Malcom MK <malcom.m@live.ca>
2017-03-16 11:59:41 -0400

    Boucles

--- exercices1.php
+++ exercices1.php
@@ -1,53 +1,23 @@
+<h1>La bouche WHILE</h1>
+<br>
+
 <?php
-$age = 12;
-$langue= "anglais";
-$sexe="fille";
-$variable="23";
+$nombre_de_lignes = 1;
+
+while ($nombre_de_lignes <= 15)
+{
+    echo 'En utilisant whille,ceci est la ligne n°' . $nombre_de_lignes . '<br />';
+    $nombre_de_lignes++;
+}
+?>
+
+ <br> <br>
  
-
-if ($age <= 12 AND $langue == "français")
-{
-    echo "Bienvenue sur mon site !";
-}
-elseif ($age <= 12 AND $langue == "anglais")
-{
-    echo "Welcome to my website!";
-}
-
-else  {
-	echo "Ouf t'es vieux";
-}
-
-
-?>
-<hr>
-<!-- ========================================================================= -->
-<h4>Avec OR</h4>
+ <h1>La bouche WHILE</h1>
 
 <?php
-if ($sexe == "fille" OR $sexe == "garçon")
+for ($nombre_de_lignes = 1; $nombre_de_lignes <= 15; $nombre_de_lignes++)
 {
-    echo "Salut Terrien !";
-}
-else
-{
-    echo "Euh, si t'es ni une fille ni un garçon, t'es quoi alors ?";
+    echo 'En utilisant For, ceci est la ligne n°' . $nombre_de_lignes . '<br />';
 }
 ?>
-<hr>
-
-<!-- =========================================================================== -->
-<h4>Bonus</h4>
-
-<?php
-if ($variable == 23)
-{
-?>
-<strong>Bravo !</strong> Vous avez trouvé le nombre mystère !
-<?php
-}
-?>
-<!-- on n'a pas utilisé deecho. En effet, il vous suffit d'ouvrir l'accolade ({), puis de fermer la balise PHP (?>), et vous pouvez mettre tout le texte à afficher que vous voulez en HTML !
-Rudement pratique quand il y a de grosses quantités de texte à afficher, et aussi pour éviter d'avoir à se prendre la tête avec les antislashs devant les guillemets ("ou'). -->
-
-<!-- =========================================================================== -->
